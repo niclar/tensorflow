01ec0aaf553d4ff69fe29ccc2a2f534c81ab8c6e
Niclas Larsén <niclar80@gmail.com>
2015-11-18 10:24:53 +0100

    commented out macro collisions

--- vs/core/unistd.h
+++ vs/core/unistd.h
@@ -12,8 +12,10 @@
 #include <process.h> /* for getpid() and the exec..() family */
 #include <direct.h> /* for _getcwd() and _chdir() */
 
+/*
 #define srandom srand
 #define random rand
+*/
 
 /* Values for the second argument to access.
    These may be OR'd together.  */
